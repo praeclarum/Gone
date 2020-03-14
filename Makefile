54a6ae7b67f8211545e49d52636a2ad0766a35e2
Frank A. Krueger <fak@praeclarum.org>
2020-03-14 11:29:04 -0700

    Add Parser

---
+++ Makefile
@@ -0,0 +1,17 @@
+
+
+
+Parser/Parser.Generated.cs: Parser/skeleton.cs.templ Parser/Parser.jay
+	cd Parser && ./jay -c Parser.jay < skeleton.cs.templ > Parser.Generated.cs
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Parser/Parser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Gone.Parser
+{
+    public partial class GoParser
+    {
+        int yacc_verbose_flag = 0;
+
+        public GoParser()
+        {
+        }
+    }
+}
